bb8f4c2805cbaa7859378954dd014def3cb77dd2
VAUBAN_C2\Administrateur <bounouhconseils@gmail.com>
2023-06-14 09:57:41 +0200

    first commit

---
+++ src/main/java/usine/Transformer.java
@@ -0,0 +1,102 @@
+package usine;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Transformer {
+	
+	HashMap <String,String> tokens= new HashMap <String, String>();  
+/**
+ * transforms a text by repacing a set of tokens by pre-dfined values.
+ * 
+ * example :
+ * 
+ * I m {name} => I am Robert
+ * 
+ * A token should be a name between brackets : {token}.
+ */
+	
+    public Transformer () {
+    	
+    
+    	/**
+    	 * Adds a new token which will used when transforming 
+    	 * 
+    	 * @param token The token, without the brackets : "name".
+    	 * @param value  the value 
+    	 */
+		
+	}
+    
+    
+	public void addToken(String token, String value) 
+	{
+		
+		tokens.put(token,value);
+	
+	}
+	
+	
+	/*
+	 * Transforme a text by replacing the token with their corresponding value.
+	 * Each token should be between brackets, for example:
+	 * hello {name}, how are tou doing?
+	 * 
+	 * @param text The take which 
+	 * @param value  Transformed text.
+	 * 
+	 */
+	public String transform( String text) {
+		
+		String transformedText = text;
+		
+		for(Entry<String,String> entry : tokens.entrySet())
+		{
+			transformedText = transformedText.replace(entry.getKey(), entry.getValue());
+		}
+        return transformedText ;
+    }
+	
+	
+	public String transform1( String text) 
+	{
+		
+		StringBuilder stringBuilder = new StringBuilder(); 
+		
+		Pattern pattern = Pattern.compile("\\{(.*?)\\}");
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find())
+		{
+			String token = matcher.group(1);
+			
+			String value = tokens.get(token);
+			
+			if( value != null)
+			
+				matcher.appendReplacement(stringBuilder, value);
+			else
+
+				matcher.appendReplacement(stringBuilder, "{" + token + "}");			
+		}
+		return stringBuilder.toString() ;
+	}
+	
+	public static void main(String[] args) {
+        String message = "This is just a message to {name} who lives at {address} and works at {company}, you can call on {phone}";
+
+        Transformer transformer = new Transformer();
+        // Transformer georgeTransformer = new Transformer("name", "address", "phone",
+        // "company");
+
+        transformer.addToken("name", "George");
+        transformer.addToken("address", "78, rue Will Smith");
+        transformer.addToken("phone", "03 54 87 69 88");
+        transformer.addToken("company", "Amazon");
+        System.out.println(transformer.toString());
+        System.out.println(transformer.transform1(message));
+    }
+
+	  
+}
